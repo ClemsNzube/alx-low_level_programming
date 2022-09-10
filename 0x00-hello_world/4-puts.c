67669cc6b239f49473da374979f23f8acbd953a4
ClemsNzube <clemsnzube555@gmail.com>
2022-09-10 03:30:04 -0700

    First C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * C program that prints a text with a new line and
+ * returns the value 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+}
