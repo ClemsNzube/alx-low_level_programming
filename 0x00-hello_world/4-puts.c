594eca40edc8f09c826db02cfd09efc305ee5f7c
ClemsNzube <102387202+ClemsNzube@users.noreply.github.com>
2022-09-10 11:41:32 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Description: C program that prints a text with a new line
  * Return: 0
  * Also uses puts instead of printf
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
