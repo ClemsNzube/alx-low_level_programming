46400e54f6655f84a635bd17badc506b11fcaa88
ClemsNzube <clemsnzube555@gmail.com>
2022-09-12 09:25:45 -0700

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print all lower case alphabet except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c==e && c==q)
+		{
+			continue;
+		}
+		putchar (c);
+		c++;
+	}
+	putchar ('\n');
+	return (0);
+}
