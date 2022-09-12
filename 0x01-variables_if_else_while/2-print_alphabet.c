cad2ce0e884832893a3ccc3114b7a03c2e4cd32b
ClemsNzube <clemsnzube555@gmail.com>
2022-09-12 08:40:16 -0700

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: A program that prints alphabet in lowercase
+ * Return: 0
+ */
+int main<void>
+{
+	char c='a';
+	while (c <= 'z')
+	{
+		putchar ("%c\n", c);
+	}
+	return (0)
