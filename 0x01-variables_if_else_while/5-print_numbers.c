40676c0b9b26aa4ab4cded4808eb621a0ad46458
ClemsNzube <clemsnzube555@gmail.com>
2022-09-12 09:53:15 -0700

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: Output numbers 0 - 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf ("%d", i);
+		i++;
+	}
+	printf ("\n");
+	return (0);
+}
