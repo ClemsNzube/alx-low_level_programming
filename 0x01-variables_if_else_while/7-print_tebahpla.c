1316c961397be8770a41da5b3a6022ede6966046
ClemsNzube <clemsnzube555@gmail.com>
2022-09-12 10:19:51 -0700

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * Description: prints alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}	
