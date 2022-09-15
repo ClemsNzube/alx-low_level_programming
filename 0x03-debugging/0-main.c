59376f7d26b576c399b9662b52ac4307d69c9437
ClemsNzube <clemsnzube555@gmail.com>
2022-09-15 10:06:35 -0700

    Update 0-main.c because of bettt error

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main - test that the function positive_or_negative() gives the correct output when given a case of 0
+ * main - test that the function positive_or_negative()
+ * gives the correct output when given a case of 0
  * Return: Always 0
  */
 
