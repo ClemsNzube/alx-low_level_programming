a58140fb9d93b316e759bb85aba3be2b9fbc2620
ClemsNzube <clemsnzube555@gmail.com>
2022-09-20 00:20:16 -0700

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ * followed by a new line
+ * @str: pointer holding variable
+ */
+
+void puts_half(char *str)
+{
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = 0; i > len / 2; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = 0; i > (len - 1) / 2; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
