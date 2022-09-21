32c71350539980e037f8af7e63b023ce44e10668
ClemsNzube <clemsnzube555@gmail.com>
2022-09-21 14:35:33 -0700

    Update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,26 +9,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len1, len2, i;
+	int len, i;
 
-	len1 = 0;
-	len2 = 0;
+	len = 0;
 
 	while (dest[len1] != '\0')
 	{
 		len1++;
 	}
 
-	while (src[len2] != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		len2++;
+		dest[len] = src[i];
+		len++;
 	}
-
-	for (i = 0; i < len2; i++)
-	{
-		dest[len1 + i] = src[i];
-	}
-	dest[i] = '\0';
+	dest[len] = '\0';
 
 	return (dest);
 }
