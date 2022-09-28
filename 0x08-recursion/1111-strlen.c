cc1abf01d95be695256a20c540827ec282a6f270
Clement Nzubechukwu <102387202+ClemsNzube@users.noreply.github.com>
2022-09-28 22:38:00 +0100

    Delete 1111-strlen.c

--- 0x08-recursion/1111-strlen.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-/**
- * _strlen - return length of string
- * @str: string to check
- *                                                     * Return: length of str
- */                                                   int _strlen(char *str)                                {                                                             if (*str == '\0')                                             return (0);                                   else
-                return (1 + _strlen(str + 1));
-}
