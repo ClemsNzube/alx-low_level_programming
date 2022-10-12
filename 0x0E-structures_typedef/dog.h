ec9b6809c82ddb8827c59724d2fd5b5085318bdc
ClemsNzube <clemsnzube555@gmail.com>
2022-10-12 07:43:51 -0700

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * struct dog - new type
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: A new structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
