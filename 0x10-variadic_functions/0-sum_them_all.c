cd187ad734da74a356e9a363718de69d84ef84b9
ClemsNzube <clemsnzube555@gmail.com>
2022-10-15 21:17:47 -0700

    Create Files

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,17 +12,15 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	int i = 0, sum = 0;
 
-	va_start(ap, n);
+	va_start(ap, n)
 
-	if (n == 0)
+	if (n != 0)
 	{
-		return (0);
-	}
-
-	while (i < n)
-	{
-		sum += va_arg(ap, n);
-		i++;
+		while (i < n)
+		{
+			sum += va_arg(ap, unsigned int);
+			i++;
+		}
 	}
 	va_end(ap);
 	return (sum);
